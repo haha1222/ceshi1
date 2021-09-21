0927a5d01017c0a088c40444728299aed4be045e
haha1222 <42240129+haha1222@users.noreply.github.com>
2021-09-21 15:18:53 +0800

    Update ceshi1.sh

--- ceshi1.sh
+++ ceshi1.sh
@@ -23,7 +23,7 @@ sudo service sshd restar
 
 passwd
 
-wget -N bash <(curl -fsSL git.io/warp.sh) 6 && chmod +x menu.sh && ./menu.sh
+wget <(curl -fsSL git.io/warp.sh) 6 && chmod +x menu.sh && ./menu.sh
 
 wget -P /root -N --no-check-certificate "https://raw.githubusercontent.com/mack-a/v2ray-agent/master/install.sh" && chmod 700 /root/install.sh && /root/install.sh
 
