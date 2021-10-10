3c1a7f8a76e0ef30201ddf1d20a28c6f0f62291f
haha1222 <42240129+haha1222@users.noreply.github.com>
2021-10-09 22:59:20 -0700

    Update ceshi1.sh

--- ceshi1.sh
+++ ceshi1.sh
@@ -11,7 +11,7 @@ sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/s
 sudo service sshd restar
 passwd
 bash <(curl -fsSL git.io/warp.sh) 6
-wget -P /root -N --no-check-certificate "https://raw.githubusercontent.com/mack-a/v2ray-agent/master/install.sh" && chmod 700 /root/install.sh && /root/install.sh
 wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
 chmod +x shadowsocks-all.sh
 ./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
+wget -P /root -N --no-check-certificate "https://raw.githubusercontent.com/mack-a/v2ray-agent/master/install.sh" && chmod 700 /root/install.sh && /root/install.sh
